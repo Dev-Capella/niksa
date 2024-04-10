6d553464e83cc9e241ecce60c5a001ecd364457d
Emirhan Çöpoğlu <copogluemirhan@outlook.com>
2024-04-10 19:24:57 +0300

    about us updated

--- resources/react-app/src/pages/AboutUs/About.jsx
+++ resources/react-app/src/pages/AboutUs/About.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import Header from "../../components/Header/Header";
 import BreadcrumbsNav from "../../components/BreadcrumbsNav/BreadcrumbsNav";
-import Footer from "../../components/Footer/Footer";
-import AboutusComponent from "../../components/AboutusComponent/AboutusComponent";
-import SloganBanner from "../../components/SloganBanner/SloganBanner";
+import AboutusComponent from "../../components/AboutUs/AboutusComponent";
 import aboutus from "../../assets/kaynak1.jpg";
 
 const About = () => {
@@ -11,7 +8,6 @@ const About = () => {
         <>
             <BreadcrumbsNav imageSrc={aboutus} text={"Hakkımızda"} />
             <AboutusComponent />
-            <SloganBanner />
         </>
     );
 };
