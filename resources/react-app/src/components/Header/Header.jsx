815dd153045e3d34f5d7ee874f3afc6c8669dc85
Emirhan Çöpoğlu <copogluemirhan@outlook.com>
2024-04-14 15:42:21 +0300

    last

--- resources/react-app/src/components/Header/Header.jsx
+++ resources/react-app/src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import HeaderLogo from "../../assets/HeaderLogo/headerlogo.png";
-import { GrLanguage } from "react-icons/gr";
+
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const [navbar, setNavbar] = useState(false);
@@ -23,42 +24,32 @@ const Header = () => {
         <nav
             className={
                 navbar
-                    ? "flex flex-row items-center justify-center h-24 w-full gap-24 fixed top-0 z-[9999] bg-[#343280] transition-all duration-500"
+                    ? "  flex flex-row items-center justify-between h-24 w-full fixed top-0 z-[9999] bg-[#343280] transition-all duration-500"
                     : "bg-transparent transition-all duration-500"
             }
         >
-            <div className="flex flex-row items-center justify-center h-24 w-full gap-24 fixed top-0 z-[9999] bg-transparent">
+            <div className="  flex flex-row items-center justify-between h-24 w-full  fixed top-0 z-[9999] bg-transparent">
                 <img
                     className="max-w-none max-h-28 "
                     src={HeaderLogo}
                     alt="Header"
                 ></img>
                 <div>
-                    <ul className="flex flex-row justify-around items-center gap-24 ">
+                    <ul className="flex flex-row justify-around items-center gap-48 ">
                         <li className="font-bold text-base text-center text-white max-xl:text-xs w-full nav-item">
-                            <a href="/"> ANASAYFA</a>
+                            <Link to={"/"}>ANASAYFA </Link>
                         </li>
                         <li className="font-bold text-base text-center text-white max-xl:text-xs w-full nav-item">
-                            <a href="about"> HAKKIMIZDA </a>
+                            <Link to={"/product"}>ÜRÜNLER </Link>
                         </li>
+
                         <li className="font-bold text-base text-center text-white max-xl:text-xs w-full nav-item">
-                            <a href="/corporate"> KURUMSAL</a>
+                            <Link to={"/about"}>HAKKIMIZDA </Link>
                         </li>
+
                         <li className="font-bold text-base text-center text-white max-xl:text-xs w-full nav-item">
-                            <a href=""> ÜRÜNLER </a>
+                            <Link to={"/contact"}>İLETİŞİM </Link>
                         </li>
-                        <li className="font-bold text-base text-center text-white max-xl:text-xs w-full nav-item">
-                            <a href=""> HİZMETLER</a>
-                        </li>
-                        <li className="font-bold text-base text-center text-white max-xl:text-xs w-full nav-item">
-                            <a href=""> PROJELER </a>
-                        </li>
-                        <li className="font-bold text-base text-center text-white max-xl:text-xs w-full nav-item">
-                            <a href="/contact"> İLETİŞİM</a>
-                        </li>
-                        <div>
-                            <GrLanguage className="w-6 h-6" />
-                        </div>
                     </ul>
                 </div>
             </div>
